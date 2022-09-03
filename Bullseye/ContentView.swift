73404912e0f24a60a9fb8806144a3121b9272f46
Dmitrii Frolov <65853205+dmitriifrlv@users.noreply.github.com>
2022-09-03 11:56:55 -0400

    Initial Commit

---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Dmitrii Frolov on 03.09.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
